0e417376edad39e5c152578c43c46f7df0d4f118
Emerson Melo Martins <emerson25xd@gmail.com>
2022-05-19 11:22:14 -0300

    Added supertest for integration tests and adjustments

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
@@ -0,0 +1,58 @@
+import request from 'supertest';
+import { app } from '@shared/infra/http/app';
+import createConnection from '@shared/infra/typeorm';
+import { Connection } from 'typeorm';
+import { hash } from 'bcryptjs';
+import { v4 as uuid } from 'uuid';
+
+describe('Create Category Controller', () => {
+  let connection: Connection;
+
+  beforeAll(async () => {
+    connection = await createConnection();
+    await connection.runMigrations();
+
+    const id = uuid();
+    const password = await hash('1234', 8);
+
+    connection.query(
+      `INSERT INTO USERS(
+          id, name, email, password, is_admin, driver_license, created_at
+        ) VALUES(
+          '${id}',
+          'Emerson Melo',
+          'emerson25xd@gmail.com',
+          '${password}',
+          true,
+          '123abc',
+          'now()'
+        )`
+    );
+  });
+
+  afterAll(async () => {
+    await connection.dropDatabase();
+    await connection.close();
+  });
+
+  it('should be able to create a new category', async () => {
+    const responseToken = await request(app).post('/sessions').send({
+      email: 'emerson25xd@gmail.com',
+      password: '1234',
+    });
+
+    const { token } = responseToken.body;
+
+    const response = await request(app)
+      .post('/categories')
+      .send({
+        name: 'Category Supertest Name',
+        description: 'Category Supertest Desc',
+      })
+      .set({
+        Authorization: `Bearer ${token}`,
+      });
+
+    expect(response.status).toBe(201);
+  });
+});
---
+++ src/shared/infra/http/app.ts
@@ -0,0 +1,41 @@
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
+import swaggerUi from 'swagger-ui-express';
+
+import 'reflect-metadata';
+
+import { router } from '@shared/infra/http/routes';
+import swaggerJson from '../../../swagger.json';
+
+import createConnection from '@shared/infra/typeorm';
+
+import '@shared/container';
+
+import { AppError } from '@shared/errors/AppError';
+
+// Caso sem docker utilizar "localhost" como parâmetro da função abaixo
+createConnection();
+
+const app = express();
+app.use(express.json());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerJson));
+
+app.use(router);
+
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response
+        .status(err.statusCode)
+        .json({ status: err.statusCode, message: err.message });
+    }
+
+    return response.status(500).json({
+      status: 'error',
+      message: `Internal server error - ${err.message}`,
+    });
+  }
+);
+
+export { app };
