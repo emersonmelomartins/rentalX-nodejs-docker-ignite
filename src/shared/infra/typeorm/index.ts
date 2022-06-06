ec72502adcdb91cdac39a82a6e7a0a6b858b9280
Emerson Melo Martins <emerson25xd@gmail.com>
2022-06-06 16:47:53 -0300

    typeorm connection adjust to localhost without docker container reference

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -13,12 +13,25 @@ import { Connection, createConnection, getConnectionOptions } from 'typeorm';
 //   });
 // });
 
-export default async (host = 'database_ignite'): Promise<Connection> => {
+// export default async (host = 'database_ignite'): Promise<Connection> => {
+//   const defaultOptions = await getConnectionOptions();
+
+//   return createConnection(
+//     Object.assign(defaultOptions, {
+//       host: process.env.NODE_ENV === 'test' ? 'localhost' : host,
+//       database:
+//         process.env.NODE_ENV === 'test'
+//           ? 'rentx_test'
+//           : defaultOptions.database,
+//     })
+//   );
+// };
+
+export default async (): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
 
   return createConnection(
     Object.assign(defaultOptions, {
-      host: process.env.NODE_ENV === 'test' ? 'localhost' : host,
       database:
         process.env.NODE_ENV === 'test'
           ? 'rentx_test'
